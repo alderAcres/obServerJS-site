3d60b7265699af462f7a0f8d166310403e336881
Julia Collins <collins.acumed@gmail.com>
2021-07-17 15:23:33 -0700

    fixed margin issued and image alts

--- components/Feature.js
+++ components/Feature.js
@@ -9,7 +9,7 @@ const Feature = () => {
     <h1 className='headers' id='feature-header' style={{marginBottom:'10vh', textAlign:'center'}}>Features</h1>
     <div className='feature-container'>
     <div className='feature-box1'>
-        <Image src={'/app-tree.png'} width='680vw' height='440vh'/>
+        <Image src={'/dashboard1.png'} width='680vw' height='440vh'/>
           </div>
           <div className='feature-box2'>
           <h3 className= 'desc-h2' id='left-header' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Simplify Backend Routing Architecture</h3>
@@ -19,7 +19,7 @@ const Feature = () => {
           <Button id= 'button-mobile' className="btn-md" style={{textAlign: 'center'}}>Download Now</Button>
         </div> 
         <div className='feature-box' style={{marginTop:'8vh'}}>
-          <Image src={'/dashboard1.png'} width='680vw' height='440vh'/>
+          <Image src={'/dashboard1.png'} alt='dashboard' width='680vw' height='440vh'/>
        </div>
         <div className='feature-box4'>
           <h3 id='left-header' className= 'desc-h2' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Visualize The Entire App Stack</h3>
@@ -32,7 +32,7 @@ const Feature = () => {
       
         </div>
         <div className='feature-box5' style={{marginTop:'8vh'}}>
-            <Image src={'/app-tree.png'} alt='app-tree' width='680vw' height='440vh'/>
+            <Image src={'/dashboard1.png'} alt='app-tree' width='680vw' height='440vh'/>
           </div> 
           <div className='feature-box6'>
               <h3 id='left-header' className= 'desc-h2' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Test Multiple Endpoints</h3>
