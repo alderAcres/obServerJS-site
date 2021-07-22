b31b98329282313bf892a6b55f1338fc6e1d51f4
Julia Collins <collins.acumed@gmail.com>
2021-07-21 19:20:47 -0700

    full responsiveness for x-wide screens

--- components/Description.js
+++ components/Description.js
@@ -10,7 +10,7 @@ const Description = () => {
       <Image src={'/dash2.png'} alt='code-block' width='600vw' height='360vh'/>
     </div>
     <div className='desc-box2'>
-      <h2 className= 'desc-h2' style={{fontSize:'2.6vw', textAlign:'center', marginBottom:'3vh'}}> This is <span style={{color:'#8CD8BE'}}>obServerJS</span></h2>
+      <h2 className= 'headers' style={{fontSize:'2.6vw', textAlign:'center', marginBottom:'3vh'}}> This is <span style={{color:'#8CD8BE'}}>obServerJS</span></h2>
       <p className= 'desc-p' style={{fontSize:'2vw'}}>
         Download our intuitively designed desktop application to have access to the expected response, a visual represention of the entire application stack (every possible route the response object can take in the server), 
         and a testing suite to run multiple endpoints to check for errors.
