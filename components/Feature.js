d898e91239cb6d8228a8f82bb41ece9c4f4b73de
Eric Wilding <ewilding8@gmail.com>
2021-07-27 18:40:14 -0500

    Enable picture zooming and resized appropriately

--- components/Feature.js
+++ components/Feature.js
@@ -1,53 +1,61 @@
 import React from 'react'
-import {Container, Button, Row, Col } from 'react-bootstrap'; 
+import { Button } from 'react-bootstrap';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { Link } from '../routes';
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 const Feature = () => {
   return (
-<div id= 'features' className='feature-div'>
-    <h1 className='headers' id='feature-header' style={{marginBottom:'10vh', textAlign:'center'}}>Features</h1>
-    <div className='feature-container'>
-    <div className='feature-box1'>
-        <Image src={'/dashboard3.png'} width='680vw' height='440vh'/>
-          </div>
-          <div className='feature-box2'>
-          <h3 className= 'desc-h2' id='left-header' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Simplify Backend Routing Architecture</h3>
-          <p className= 'desc-p' style={{fontSize:'2vw'}}>obServerJS enables developers to simplify complex server-side code 
-          by visually displaying the exact path the response object takes given a specific url end point. Expect nothing less than 
-          full server-side transparancy with obServerJS. </p>
+    <div id='features' className='feature-div'>
+      <h1 className='headers' id='feature-header' style={{ marginBottom: '10vh', textAlign: 'center' }}>Features</h1>
+      <div className='feature-container'>
+        <div className='feature-box1'>
+          <Zoom>
+            <Image src={'/response.png'} width='1280vw' height='960vh' />
+          </Zoom>
+        </div>
+        <div className='feature-box2'>
+          <h3 className='desc-h2' id='left-header' style={{ fontSize: '2.4vw', marginBottom: '2vh' }}>Simplify Backend Routing Architecture</h3>
+          <p className='desc-p' style={{ fontSize: '2vw' }}>obServerJS enables developers to simplify complex server-side code
+            by visually displaying the exact path the response object takes given a specific url end point. Expect nothing less than
+            full server-side transparancy with obServerJS. </p>
           <Link target="_blank" route={'/#download'}><Button id='button-mobile' className='btn-md' variant='dark' >Download</Button></Link>
-        </div> 
-        <div className='feature-box' style={{marginTop:'8vh'}}>
-          <Image src={'/dash1.png'} alt='dashboard' width='680vw' height='440vh'/>
-       </div>
+        </div>
+        <div className='feature-box' style={{ marginTop: '8vh' }}>
+          <Zoom>
+            <Image src={'/dash1.png'} alt='dashboard' width='1280vw' height='960vh' />
+          </Zoom>
+        </div>
         <div className='feature-box4'>
-          <h3 id='left-header' className= 'desc-h2' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Visualize The Entire App Stack</h3>
-          <p className= 'desc-p' style={{fontSize:'2vw'}}>In addition to having access to the route specific stack, obServerJS 
-          offers a visual representation of the entire application stack in a tree of nodes, 
-          reducing complexity and enabling for a quicker understanding of all potential routes the response object can take through 
-          routing and controller files.</p>
+          <h3 id='left-header' className='desc-h2' style={{ fontSize: '2.4vw', marginBottom: '2vh' }}>Visualize The Entire App Stack</h3>
+          <p className='desc-p' style={{ fontSize: '2vw' }}>In addition to having access to the route specific stack, obServerJS
+            offers a visual representation of the entire application stack in a tree of nodes,
+            reducing complexity and enabling for a quicker understanding of all potential routes the response object can take through
+            routing and controller files.</p>
 
           <Link target="_blank" route={'/#download'}><Button id='button-mobile' className='btn-md' variant='dark' >Try It Out</Button>
           </Link>
         </div>
-        <div className='feature-box5' style={{marginTop:'8vh'}}>
-            <Image src={'/dash3.png'} alt='app-tree' width='680vw' height='440vh'/>
-          </div> 
-          <div className='feature-box6'>
-              <h3 id='left-header' className= 'desc-h2' style={{fontSize:'2.4vw', marginBottom:'2vh'}}>Test Multiple Endpoints</h3>
-              <p className= 'desc-p' style={{fontSize:'2vw'}}>To make the debugging process even more efficient, obServerJS has a 
-              testing suite that offers the options of adding multiple endpoints and and to gain information on whether or not errors 
-              are thrown.</p>
-              <motion.a  whileHover={{
-                scale: 1.2,
-                transition: { duration: 1 },
-              }} target="_blank" href='https://github.com/oslabs-beta/obServerJS'><Button id='button-mobile' className='btn-md' variant='dark' >Read The Docs</Button></motion.a>
-              </div>
-               </div>
-              </div>
-  
+        <div className='feature-box5' style={{ marginTop: '8vh' }}>
+          <Zoom>
+            <Image src={'/response.png'} alt='app-tree' width='1280vw' height='960vh' />
+          </Zoom>
+        </div>
+        <div className='feature-box6'>
+          <h3 id='left-header' className='desc-h2' style={{ fontSize: '2.4vw', marginBottom: '2vh' }}>Test Multiple Endpoints</h3>
+          <p className='desc-p' style={{ fontSize: '2vw' }}>To make the debugging process even more efficient, obServerJS has a
+            testing suite that offers the options of adding multiple endpoints and and to gain information on whether or not errors
+            are thrown.</p>
+          <motion.a whileHover={{
+            scale: 1.2,
+            transition: { duration: 1 },
+          }} target="_blank" href='https://github.com/oslabs-beta/obServerJS'><Button id='button-mobile' className='btn-md' variant='dark' >Read The Docs</Button></motion.a>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
--- components/Intro.js
+++ components/Intro.js
@@ -10,54 +10,54 @@ const Intro = () => {
   const [newName, setnewName] = useState("");
 
   const shuffle = useCallback(() => {
-      const index = Math.floor(Math.random() * names.length);
-      setnewName(names[index]);
+    const index = Math.floor(Math.random() * names.length);
+    setnewName(names[index]);
   }, []);
   useEffect(() => {
-      const intervalID = setInterval(shuffle, 2000);
-      return () => clearInterval(intervalID);
+    const intervalID = setInterval(shuffle, 2000);
+    return () => clearInterval(intervalID);
   }, [shuffle])
 
   return (
     <div className='intro-container'>
-    <div className='intro-box1'>
-        <motion.h1 whileHover = {{
+      <div className='intro-box1'>
+        <motion.h1 whileHover={{
           scale: 1.2,
           transition: {
             duration: .2
           }
-        }} id ='header-intro' style={{fontSize:'4vw', color:'#333333',  display:'inline-block', whiteSpace:'nowrap' }}>Less Time Debugging</motion.h1>
-        <motion.h1 id ='header-intro' whileHover ={{
+        }} id='header-intro' style={{ fontSize: '4vw', color: '#333333', display: 'inline-block', whiteSpace: 'nowrap' }}>Less Time Debugging</motion.h1>
+        <motion.h1 id='header-intro' whileHover={{
           scale: 1.2,
           transition: {
             duration: .2
           }
-        }} style={{fontSize:'4vw', color:'#333333', display:'inline-block', whiteSpace:'nowrap' }}>More Time<motion.span>{` ${newName}`}</motion.span>
+        }} style={{ fontSize: '4vw', color: '#333333', display: 'inline-block', whiteSpace: 'nowrap' }}>More Time<motion.span>{` ${newName}`}</motion.span>
         </motion.h1>
-  
-      <h2 id='p-intro' style={{fontSize:'1.9vw', letterSpacing: '.04rem'}}>obServerJS saves developers time by providing transparent express routing and a smoother debugging process.</h2>
-      <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'2vh'}}>
-      <Link target="_blank" route={'/#download'}><Button id='button-mobile' className='btn-md' variant='dark' >Download</Button></Link>
-      </div>
-      <motion.div initial='hidden' animate='visible' variants={{
-        hidden: {
-          scale: 0.8,
-          opacity: 0
-        },
-        visible: {
-          scale: 1.2,
-          opacity: 1,
-          transition: {
-            delay: .8
+
+        <h2 id='p-intro' style={{ fontSize: '1.9vw', letterSpacing: '.04rem' }}>obServerJS saves developers time by providing transparent express routing and a smoother debugging process.</h2>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2vh' }}>
+          <Link target="_blank" route={'/#download'}><Button id='button-mobile' className='btn-md' variant='dark' >Download</Button></Link>
+        </div>
+        <motion.div initial='hidden' animate='visible' variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0
+          },
+          visible: {
+            scale: 1.2,
+            opacity: 1,
+            transition: {
+              delay: .8
+            }
           }
-        }
-      }}>
-       </motion.div> 
-       </div>
-       <div className='intro-box2'>
-     <Image id='computer' src='/macbook-main.png' alt='computer image' height='400vh' width='400vw' />
-     </div>
-     </div>
+        }}>
+        </motion.div>
+      </div>
+      <div className='intro-box2'>
+        <Image id='computer' src='/macbook-main.png' alt='computer image' height='400vh' width='400vw' />
+      </div>
+    </div>
   )
 }
 
