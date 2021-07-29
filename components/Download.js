954e39a75cef292fa5c7195a265ce964ebf10554
Eric Wilding <ewilding8@gmail.com>
2021-07-28 19:16:03 -0500

    Enable Windows download link

--- components/Download.js
+++ components/Download.js
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faApple, faMicrosoft } from '@fortawesome/free-brands-svg-icons'
 const Download = () => {
   return (
- 
+
     <div id='download'>
-    <h2 className='download-h2'>Download</h2>
-    <div className='download-container'>
-    <a target="_blank" href='https://observerjs-mac-build.s3.amazonaws.com/obServerJS-0.1.0.pkg'><FontAwesomeIcon id='icon' style={{height:'9vh', width:'auto'}} icon={faApple}/></a>
-    <a target="_blank" href='https://github.com/oslabs-beta/obServerJS'><FontAwesomeIcon id='icon' style={{height:'8vh', width:'auto'}} icon={faMicrosoft}/></a>
-    </div>
+      <h2 className='download-h2'>Download</h2>
+      <div className='download-container'>
+        <a target="_blank" href='https://observerjs-mac-build.s3.amazonaws.com/obServerJS-0.1.0.pkg'><FontAwesomeIcon id='icon' style={{ height: '9vh', width: 'auto' }} icon={faApple} /></a>
+        <a target="_blank" href='https://observerjs-windows-build.s3.amazonaws.com/obServerJS+Setup+0.1.0.exe'><FontAwesomeIcon id='icon' style={{ height: '8vh', width: 'auto' }} icon={faMicrosoft} /></a>
+      </div>
     </div>
 
   )
